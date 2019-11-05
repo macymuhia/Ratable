3ebfbe788eed69e14dd6de300d2e37ac063dd86a
macymuhia <macymuhia@gmail.com>
2019-11-06 00:20:22 +0300

    Add profile edit view function

--- users/views.py
+++ users/views.py
@@ -16,3 +16,44 @@ def profile(request):
         "registration/profile.html",
         {"user_data": user_data, "user_profile": user_profile},
     )
+
+
+@login_required(login_url="")  # only logged in users should access this
+def edit_profile(request):
+
+    current_user = request.user
+    user = User.objects.get(id=current_user.id)
+
+    # prepopulate UserProfileForm with retrieved user values from above.
+    user_form = UserForm(instance=user)
+
+    # The sorcery begins from here, see explanation below
+    ProfileInlineFormset = inlineformset_factory(
+        User, UserProfile, fields=("photo", "phone", "bio")
+    )
+    formset = ProfileInlineFormset(instance=user)
+
+    if request.user.is_authenticated and request.user.id == user.id:
+        if request.method == "POST":
+            user_form = UserForm(request.POST, request.FILES, instance=user)
+            formset = ProfileInlineFormset(
+                request.POST, request.FILES, instance=user)
+
+            if user_form.is_valid():
+                created_user = user_form.save(commit=False)
+                formset = ProfileInlineFormset(
+                    request.POST, request.FILES, instance=created_user
+                )
+
+                if formset.is_valid():
+                    created_user.save()
+                    formset.save()
+                    return redirect("profile")
+
+        return render(
+            request,
+            "registration/edit_profile.html",
+            {"noodle": user.id, "noodle_form": user_form, "formset": formset},
+        )
+    else:
+        raise PermissionDenied
