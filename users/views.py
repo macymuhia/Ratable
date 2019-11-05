529b2e08becc47fd2256077a1aef6331691f70e2
macymuhia <macymuhia@gmail.com>
2019-11-06 00:18:04 +0300

    Add user profile view function

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import UserProfile
 
 # Create your views here.
+@login_required(login_url="")
+def profile(request):
+
+    current_user = request.user
+    user_data = User.objects.get(id=current_user.id)
+    user_profile = UserProfile.objects.get(id=current_user.id)
+
+    return render(
+        request,
+        "registration/profile.html",
+        {"user_data": user_data, "user_profile": user_profile},
+    )
