22d2670a16851e07e9a8e5fb3000fce4e6b155b6
macymuhia <macymuhia@gmail.com>
2019-11-05 06:09:30 +0300

    Add bootstrap3 and users to installed apps in the settings file

--- Ratable/settings.py
+++ Ratable/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party
+    'bootstrap3',
+
+    # Own apps
+    'users',
 ]
 
 MIDDLEWARE = [
