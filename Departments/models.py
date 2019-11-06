8b32af6da2867e36688af146a318b458ed10e1b4
kathure <juliekmuthee@gmail.com>
2019-11-06 11:06:51 +0300

    add save department function

--- Departments/models.py
+++ Departments/models.py
@@ -5,3 +5,12 @@ from django.contrib.auth.models import User
 class departments(models.Model):
     name = models.CharField(max_length=60)
     
+    def __str__(self):
+        return self.name
+
+    def save_departments(self):
+        self.save()
+
+    # def delete_departme(self):
+    #     self.delete()    
+    
