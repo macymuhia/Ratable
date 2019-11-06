0b902329c878a9dcbe8ce8f817aa73ad0c13ea79
kathure <juliekmuthee@gmail.com>
2019-11-06 15:32:51 +0300

    import settings

--- Departments/urls.py
+++ Departments/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.conf.urls import path,include
 from . import views
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
