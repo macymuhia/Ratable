f185b19d6cdfa76e06981def46304d831c81b91c
naomi <siguneema@gmail.com>
2019-11-06 14:57:58 +0300

    crate an indicator class to add all the information and functionality needed for the item

--- KPIs/models.py
+++ KPIs/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class Area(model.Model):
-    name = models.CharField(max_length=50)
+class Area(models.Model):
+    name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
@@ -20,10 +20,28 @@ class Area(model.Model):
         method used the delete the areas added.
         '''
         self.delete()
-        
+
 
     def update_area(self):
         '''
         method used the update the areas added.
         '''
         self.update()
+
+class Indicators(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_indica(self):
+        self.save()
+    
+    def update_indica(self):
+        self.update()
+    
+    def delete_indica(self):
+        self.delete()
+
+
+
