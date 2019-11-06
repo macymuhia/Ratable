fcb7001c69376184a49ce9d3e507015534672a74
naomi <siguneema@gmail.com>
2019-11-06 11:47:44 +0300

    add the area model to add all the area speciications needed

--- KPIs/models.py
+++ KPIs/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Area(model.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_area(self):
+        '''
+        method used the save the areas added.
+        '''
+        self.save()
+
+    def delete_area(self):
+        '''
+        method used the delete the areas added.
+        '''
+        self.delete()
+
+    def update_area(self):
+        '''
+        method used the update the areas added.
+        '''
+        self.update()
