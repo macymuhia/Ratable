19614984312f958f5858297c8e2c955144d7c3e7
kathure <juliekmuthee@gmail.com>
2019-11-06 22:10:41 +0300

    add a post request

--- Departments/views.py
+++ Departments/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect,get_list_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse,Http404
-from .models import Departments
+from .models import Department
 from .forms import DepartmentCreationForm
 
 
@@ -11,5 +11,18 @@ from .forms import DepartmentCreationForm
 def index(request):
     name = 'departments'
     current_user = request.user
-    if request method == 'POST'
-    form = departmentCreationForm()
+    if request.method == 'POST':
+        form = departmentCreationForm(request.POST)
+        if form.is_valid():
+            department = form.save(commit=False)
+            department.user = current_user
+            department.save()
+            return redirect('home')
+        else:
+           form = DepartmentCreationForm()
+        return render(request,
+                      'department.html', 
+                      {"user":current_user,
+                        "comment_form":form}
+                )
+   
