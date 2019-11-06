8703b1607e44e92830640e7856804940a780818b
kathure <juliekmuthee@gmail.com>
2019-11-06 18:48:52 +0300

    modified departments

--- Departments/admin.py
+++ Departments/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Departments
+from .models import Department
 
 # Register your models here.
-admin.site.register(Departments)
+admin.site.register(Department)
--- Departments/views.py
+++ Departments/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_list_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse,Http404
+from .models import Departments
+from .forms import DepartmentCreationForm
+
 
 # Create your views here.
+@login_required(login_url='account/login/')
+def index(request):
+    name = 'departments'
+    current_user = request.user
+    if request method == 'POST'
+    form = departmentCreationForm()
