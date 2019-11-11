2264f8939208efc9848830609960cdd9251d630b
tinatasha <tashambiti@gmail.com>
2019-11-11 16:07:29 +0300

    add kpi.html template file

--- KPIs/views.py
+++ KPIs/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def kpis(request):
+    return render(request, 'kpis.html', {"kpis":kpis})
--- Ratable/urls.py
+++ Ratable/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signin', include('users.urls')),
-    path('', include('dashboard.urls')),
+    path('dashboard', include('dashboard.urls')),
+    path('', include('kpis.urls')),
 ]
