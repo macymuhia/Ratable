15ce0289aa9f9cbe1234d7cb4882e89fdf3dfed4
tinatasha <tashambiti@gmail.com>
2019-11-14 12:20:50 +0300

    updating dashboard page

--- KPIs/urls.py
+++ KPIs/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.kpis, name="kpis"),
+    path("", views.graphs, name="graphs"),
 ]
--- KPIs/views.py
+++ KPIs/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def kpis(request):
     return render(request, 'kpis.html', {"kpis":kpis})
+
+def graphs(request):
+    return render(request, 'reports.html', {"reports":reports})
