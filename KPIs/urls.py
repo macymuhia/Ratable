4a2fb1024b837caeb6d0608e9000550439ca7cf8
naomi <siguneema@gmail.com>
2019-11-14 17:31:18 +0300

    add the include function to include the urls for the app

---
+++ KPIs/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+
+# from django.urls import path, include
+
+urlpatterns=[
+    path('',views.welcome,name = 'welcome'),
+    path('score/',views.score,name='score'),
+    path('area/new/', views.new_area, name='new_area'),
+    path('areas',views.areas, name='areas'),
+    path('indicator/new/', views.new_indicator, name='new_indicator'),
+]
