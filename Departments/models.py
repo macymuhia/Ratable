0ca7a2b632430bf144918dc2ef80b3f755e982a4
macymuhia <macymuhia@gmail.com>
2019-11-14 11:37:51 +0300

    Remove line manager field from department models

--- Departments/models.py
+++ Departments/models.py
@@ -2,20 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class Department(models.Model):
     name = models.CharField(max_length=60, blank=False)
-    line_manager = models.CharField(max_length=60, blank=False)
-    
-    
+
     def __str__(self):
         return self.name
 
-    def save_departments(self):
-        self.save()
+    # def save_departments(self):
+    #     self.save()
 
-    def delete_departments(self):
-        self.delete()  
-        
-    def update_departments(self):
-        self.update()  
-    
+    # def delete_departments(self):
+    #     self.delete()
+
+    # def update_departments(self):
+    #     self.update()
