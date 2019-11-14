b51cce9f1af6e7cd70d180d5e2f65593166933c0
naomi <siguneema@gmail.com>
2019-11-14 17:14:34 +0300

    create a view for the areas and indicator to be viewed

--- KPIs/views.py
+++ KPIs/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
-from forms import *
-from models import *
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from .models import *
 
 # Create your views here.
 
-@login_required(login_url='/accounts/login')
-def area(request):
 
-    profile = Profile.objects.get(user = request.user)
+def welcome(request):
+    areas = Area.objects.all()
+    indicators = Indicators.objects.all()
+    print(indicators)
+    return render (request, 'home.html',{"areas":areas,"indicators":indicators})  
+
+@login_required
+def score(request):
+    current_user = request.user 
+    return render (request, 'score.html')
+
+
+def new_area(request):
     
     if request.method == 'POST':
         
@@ -16,7 +28,6 @@ def area(request):
         if form.is_valid():
             
             post = form.save(commit = False)
-            post.user = request.user
             post.save()
             
         return redirect('/')
@@ -25,4 +36,29 @@ def area(request):
         
         form = AddArea()
         
-    return render(request, 'new_area.html', {"form": form})    
+    return render(request,'addarea.html',{"form":form}) 
+
+
+def areas(request):
+    areas = Area.objects.all()
+    return render(request,'home.html',{"areas":areas}) 
+
+
+def new_indicator(request):
+    
+    if request.method == 'POST':
+        
+        form = AddIndicator(request.POST)
+        
+        if form.is_valid():
+            
+            post = form.save(commit = False)
+            post.save()
+            
+        return redirect('/')
+    
+    else:
+        
+        form = AddIndicator()
+        
+    return render(request,'addindicator.html',{"form":form})
