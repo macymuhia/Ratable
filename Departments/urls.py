39fb6e8c48977da221039854d248ee5f805aeede
kathure <juliekmuthee@gmail.com>
2019-11-15 17:15:47 +0300

    create department-edit url andf search result url

--- Departments/urls.py
+++ Departments/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('department-edit/', views.edit_deparments, name='department-edit'),
+    path('searchresult/', views.searchresult,name='searchresult')
     
         
 ]
