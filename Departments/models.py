6f3d4e27c9d01843e6bab0647a293fb1ac7252e3
macymuhia <macymuhia@gmail.com>
2019-11-16 15:27:01 +0300

    Display all departments and route correctly

--- Departments/models.py
+++ Departments/models.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import User
 
 class Department(models.Model):
     name = models.CharField(max_length=60, blank=False)
+    description = models.TextField(
+        max_length=300, blank=True, null=True, default='')
+
+    @classmethod
+    def get_all_departments(cls):
+        return cls.objects.all()
 
     def __str__(self):
         return self.name
--- Departments/urls.py
+++ Departments/urls.py
@@ -2,12 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.departments, name="departments"),
-   
-# from django.urls import path, include
-# from django.conf import settings
-# from Departments import views
-
-# urlpatterns = [
-#     path('add_department/', views.add_department_view, name='add_department'),
+    path("", views.list_departments_view, name="list_departments"),
+    path('add_department/', views.add_department_view, name='add_department'),
 ]
