3f269c0e96c63243037447d5e2406d1f45640cc8
hillarydalie <hidalie@gmail.com>
2019-11-18 00:51:06 +0300

    Add django core validators to validate our result values.

--- KPIs/models.py
+++ KPIs/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from Departments.models import Department
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
