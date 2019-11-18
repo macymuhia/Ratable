1547bc65be68d194d3031ed43a0e48d3653e4d63
kathure <juliekmuthee@gmail.com>
2019-11-18 14:24:39 +0300

    edited dashboard url

--- Departments/urls.py
+++ Departments/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.list_departments_view, name="list_departments"),
     path('add_department/', views.add_department_view, name='add_department'),
+    path('search/', views.search, name='search'),
 ]
