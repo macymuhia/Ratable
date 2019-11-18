435d55f86d4bf98cf626b1659b596942ff01d4a5
naomi <siguneema@gmail.com>
2019-11-18 14:06:49 +0300

    save all the changes made

--- Ratable/urls.py
+++ Ratable/urls.py
@@ -16,12 +16,20 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('dashboard.urls')),
     path('users/', include('users.urls')),
-    path('dashboard/', include('dashboard.urls')),
     path('departments/', include("Departments.urls")),
-    path('kpis/', include('KPIs.urls')),
-
+    path('KPIs/', include('KPIs.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
